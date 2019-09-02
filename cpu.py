42caf96c834ec5da06405d277543a8acf3a57a34
pghrogue <jenbellpgh@gmail.com>
2019-09-01 23:50:59 -0400

    Fix logic bug with handle_jne (int vs str) and cleared out debug prints

--- cpu.py
+++ cpu.py
@@ -68,8 +68,8 @@ class CPU:
         """ALU operations."""
         operand_a = self.ram_read(self.pc + 1)
         operand_b = self.ram_read(self.pc + 2)
-        print(f"DEBUG:: operand_a: {operand_a} operand_b: {operand_b}")
-        print(f"DEBUG:: reg.operand_a: {self.reg[operand_a]} reg.operand_b: {self.reg[operand_b]}")
+        # print(f"DEBUG:: operand_a: {operand_a} operand_b: {operand_b}")
+        # print(f"DEBUG:: reg.operand_a: {self.reg[operand_a]} reg.operand_b: {self.reg[operand_b]}")
         self.alutable = {}
         self.alutable[ADD] = self.reg[operand_a] + self.reg[operand_b]
         self.alutable[MUL] = self.reg[operand_a] * self.reg[operand_b]
@@ -84,22 +84,22 @@ class CPU:
                 self.flags = 0b00000100
             else:
                 self.flags = 0b00000010
-            print(f"DEBUG:: flags = {format(self.flags, '08b')}")
+            # print(f"DEBUG:: flags = {format(self.flags, '08b')}")
             
         else:
-            print("DEBUG: alu else")
+            # print("DEBUG: alu else")
             self.handle_alu(operand_a, self.alutable[IR])
 
     def handle_alu(self, operand_a, val):
-        print(f"DEBUG:: handle_alu op_a {operand_a} val {val}")
+        # print(f"DEBUG:: handle_alu op_a {operand_a} val {val}")
         self.reg[operand_a] = val
 
     def handle_ldi(self, operand_a, operand_b):
-        print(f"DEBUG:: self.reg[{operand_a}] = {operand_b}")
+        # print(f"DEBUG:: self.reg[{operand_a}] = {operand_b}")
         self.reg[operand_a] = operand_b
 
     def handle_prn(self, operand_a, operand_b):
-        print(f"DEBUG:: self.reg[{operand_a}] = {self.reg[operand_a]}")
+        # print(f"DEBUG:: self.reg[{operand_a}] = {self.reg[operand_a]}")
         print(self.reg[operand_a])
         # exit(0)
 
@@ -144,27 +144,28 @@ class CPU:
 
     def handle_jmp(self, operand_a, operand_b):
         jump_addr = self.reg[operand_a]
-        print(f"Jumping to {bin(jump_addr)}")
+        # print(f"DEBUG: Jumping to {bin(jump_addr)}")
         self.pc = jump_addr
 
     def handle_jeq(self, operand_a, operand_b):
         # If equal flag is set (true), jump to the address stored in the given register.
         if self.flags == 0b00000001:
-            print("DEBUG: Is equal")
+            # print("DEBUG: Is equal")
             self.handle_jmp(operand_a, operand_b)
         else:
-            print("DEBUG: Is not equal")
+            # print("DEBUG: Is not equal")
             self.pc += 2
         
 
     def handle_jne(self, operand_a, operand_b):
         # If E flag is clear (false, 0), jump to the address stored in the given register.
         flag_str = format(self.flags, '08b')
-        if flag_str[7:8] == 0:
-            print("DEBUG: JNE is true")
+        # print(f"DEBUG: flag_str = {flag_str} last bit = {flag_str[7:8]}")
+        if flag_str[7:8] == "0":
+            # print("DEBUG: JNE is true")
             self.handle_jmp(operand_a, operand_b)
         else:
-            print("DEBUG: JNE is false")
+            # print("DEBUG: JNE is false")
             self.pc += 2
 
     def trace(self):
@@ -198,20 +199,20 @@ class CPU:
             
             #ir_str = str(bin(IR))[2:]
             ir_str = format(IR, '08b')
-            print(f"DEBUG: ir_str = {ir_str}")
+            # print(f"DEBUG: ir_str = {ir_str}")
 
             # AAxxxxxx = operation size
             op_size = int(ir_str[:2], 2)
-            print(f"DEBUG: op_size = {op_size}")
+            # print(f"DEBUG: op_size = {op_size}")
 
             # xxBxxxxx = alu operation
             alu_set = (int(ir_str[2:3], 2) == 1)
-            print(f"DEBUG: alu_set = {alu_set}")
+            # print(f"DEBUG: alu_set = {alu_set}")
 
             # xxxCxxxx = ins_set
             ins_set = (int(ir_str[3:4], 2) == 1)
             # ins_set = ((IR >> 4) == 1)
-            print(f"DEBUG: ins_set = {ins_set}")
+            # print(f"DEBUG: ins_set = {ins_set}")
 
             if alu_set:
                 self.alu(IR)
